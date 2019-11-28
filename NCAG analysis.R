ab8a1e75cbba582d1cc560638a59546a36fc47b8
Garth <gcov89@gmail.com>
2019-11-28 13:01:40 -0500

    Nov. 28

--- NCAG analysis.R
+++ NCAG analysis.R
@@ -7,6 +7,7 @@ library(dplyr)
 
 field_data <- read.csv("NCAG_field_data.csv", header = TRUE)
 plankton_tows <- read.csv("plankton_tow_counts.csv", header = TRUE)
+PT_field <- read.csv("PT_field_data.csv", header = TRUE)
 
 ## Clean up data and merge
 
@@ -63,3 +64,79 @@ plankton_tows$shape <- mapvalues(plankton_tows$shape,
 summary(plankton_tows$shape)
 
 summary(plankton_tows$raman.ID)
+
+plankton_tows$particle.type <- 
+  mapvalues(plankton_tows$raman.ID,
+            from = levels(plankton_tows$raman.ID),
+            to = c('Unknown',
+                   'Synthetic Polymer',
+                   'Natural Anthropogenic',
+                   'Unknown Anthropogenic',
+                   'Natural',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Synthetic Polymer',
+                   'Natural',
+                   'Natural',
+                   'Unknown'))
+summary(plankton_tows$particle.type)
+
+summary(plankton_tows$colour)
+
+## Clean up plankton tow field data 
+
+summary(PT_field$sample.volume)
+
+PT <- left_join(plankton_tows, PT_field, by = 'ID')
+
+PT$ID <- as.factor(PT$ID)
+
+head(PT)
+
+PT$num <- ifelse(is.na(PT$length), 0, 1)
+
+## Separate blanks data
+
+PT_blanks <- subset(PT, sample.type == 'Blanks')
+PT <- subset(PT, sample.type == 'Plankton tows')
+
+PT_polymer <- 
+  PT %>% 
+  group_by(ID, site, shape, particle.type, raman.ID, sample.volume) %>% 
+  summarize(count = sum(num))
+
+PT_polymer$concentration <- with(PT_polymer, count/sample.volume)*1000
+
+PT_particle_type <- 
+  PT %>% 
+  group_by(ID, site, shape, sample.volume, particle.type) %>% 
+  summarize(count = sum(num))
+
+PT_particle_type$concentration <- 
+  with(PT_particle_type, 
+       count/sample.volume)*1000
+
+PT_polymer_summary <-
+  PT_polymer %>%
+  group_by(site, shape, raman.ID) %>%
+  summarize(mean = mean(concentration),
+            sd = sd(concentration))
+
+PT_particle_type_summary <-
+  PT_particle_type %>% 
+  group_by(site, shape, particle.type) %>% 
+  summarize(mean = mean(concentration),
+            sd = sd(concentration))
+  
+ggplot(PT_particle_type_summary) + 
+  geom_col(aes(x = site,
+               y = mean),
+           colour = 'black',
+           size = 1) + 
+  labs(x = 'Particles/1000 L',
+       y = 'Site') +
+  facet_grid(shape ~ particle.type)
